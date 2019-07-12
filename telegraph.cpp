5302151419b053e89849463b6aff23c7f2a863b5
Mike Dougherty <40866764+michaelpdougherty@users.noreply.github.com>
2019-07-12 12:41:31 -0500

    Completed cpp file

--- telegraph.cpp
+++ telegraph.cpp
@@ -25,6 +25,48 @@ Telegraph::Telegraph(const int output_pin, const int dit_length) {
 }
 
 void Telegraph::output_code(const char* code) {
-  const unsigned int code_length = strlen(
+  const unsigned int code_length = strlen(code);
+
+  for (unsigned int i = 0; i < code_length; i++) {
+    if (code[i] == '.')
+      dit();
+    else
+      dah();
+    if (i != code_length - 1)
+      delay(_dit_length);
+  }
+}
+
+void Telegraph::dit() {
+  Serial.print(".");
+  output_symbol(_dit_length);
+}
+
+void Telegraph::dah() {
+  Serial.print("-");
+  output_symbol(_dah_length);
+}
+
+void Telegraph::output_symbol(const int length) {
+  digitalWrite(_output_pin, HIGH);
+  delay(length);
+  digitalWrite(_output_pin, LOW);
+}
+
+void Telegraph::send_message(const char* message) {
+  for (unsigned int i = 0; i < strlen(message); i++) {
+    const char current_char = toupper(message[i]);
+    if (isalpha(current_char)) {
+      output_code(LETTERS[current_char - 'A']);
+      delay(_dah_length);
+    } else if (isdigit(current_char)) {
+      output_code(DIGITS[current_char - '0']);
+      delay(_dah_length);
+    } else if (current_char == ' ') {
+      Serial.print(" ");
+      delay(_dit_length * 7);
+    }      
+  }
+  Serial.println();
 }
 
--- telegraph.ino
+++ telegraph.ino
@@ -1,10 +1,16 @@
+#include "telegraph.h"
+const int BAUD_RATE = 9600;
+const int OUTPUT_PIN = 12;
+const int DIT_LENGTH = 200;
+
+Telegraph telegraph(OUTPUT_PIN, DIT_LENGTH);
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(BAUD_RATE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  telegraph.send_message("Hello");
+  delay(5000);
 }
+
